8376f5dd582e43b4be5cf7c5c25d09e246e2cc84
Giovanni.iacono98 <98giovanniiacono98@gmail.com>
2024-05-23 12:02:38 +0200

    completamento file js
    inserimento linea che notifica se il task è stato completato o meno

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,12 @@
 const buttonElement = document.getElementById("bottone");
 const textElement = document.getElementById("task");
-console.log(buttonElement);
-console.log(textElement);
-inserimentoTask = function (event, elemento) {
+// console.log(buttonElement);
+// console.log(textElement);
+
+const inserimentoTask = function (event) {
   event.preventDefault();
-  console.log(event);
-  console.log(textElement.value);
+  // console.log(event);
+  // console.log(textElement.value);
   //   creazione lista non ordinata
   const divTaskElement = document.getElementById("taskList");
   const divUl = document.createElement("ul");
@@ -14,8 +15,22 @@ inserimentoTask = function (event, elemento) {
   const divLi = document.createElement("li");
   divLi.innerText = textElement.value;
   divUl.appendChild(divLi);
+  // sbarramento li quando vi si clicca con il mouse
+  inserimentoCompletato();
 };
 
-buttonElement.addEventListener("click", inserimentoTask);
+const inserimentoCompletato = function () {
+  if (document.querySelectorAll) {
+    const divLiElement = document.querySelectorAll("li");
+    console.log(divLiElement);
+    for (let index = 0; index < divLiElement.length; index++) {
+      divLiElement[index].addEventListener("click", () => {
+        divLiElement[index].classList.toggle("sbarramento");
+      });
+    }
+  }
+};
 
-// sbarramento li quando vi si clicca con il mouse
+window.addEventListener("DOMContentLoaded", () => {
+  buttonElement.addEventListener("click", inserimentoTask);
+});
